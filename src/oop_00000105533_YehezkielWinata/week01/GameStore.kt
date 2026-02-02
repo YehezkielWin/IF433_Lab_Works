87977524ca09092164d8a5586d5cfd6b9452002f
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-02 22:34:54 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000105533_YehezkielWinata/week01/GameStore.kt
+++ src/oop_00000105533_YehezkielWinata/week01/GameStore.kt
@@ -3,7 +3,22 @@ package oop_00000105533_YehezkielWinata.week01
 fun main (){
     val gameTitle:String = "Avengers End Game"
     val price: Int = 90000000
-    println(calculateDiscount(price))
+    val Discount = calculateDiscount(price)
+    val userNote: String? = null
+    printReceipt(
+        title = gameTitle,
+        finalPrice = price - Discount
+    )
+
+
+
+
 }
 
 fun calculateDiscount(price: Int): Int = if (price > 50000000) price/10 else 0
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("=== Struk Game Store ===")
+    println("Game Title: $title")
+    println("Harga Akhir : $finalPrice")
+}
