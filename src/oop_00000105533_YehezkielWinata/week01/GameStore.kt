d54c15f85107e20ce314c6f741aa96364f3b9d64
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-02 22:20:47 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000105533_YehezkielWinata/week01/GameStore.kt
+++ src/oop_00000105533_YehezkielWinata/week01/GameStore.kt
@@ -2,5 +2,8 @@ package oop_00000105533_YehezkielWinata.week01
 
 fun main (){
     val gameTitle:String = "Avengers End Game"
-    val price: Int = 9000000
+    val price: Int = 90000000
+    println(calculateDiscount(price))
 }
+
+fun calculateDiscount(price: Int): Int = if (price > 50000000) price/10 else 0
