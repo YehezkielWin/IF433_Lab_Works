d08a15d4723b90dc198f729f4f1820de17cad606
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-02 16:41:26 +0700

    week01: initial setup and procedural code

---
+++ src/oop_00000105533_YehezkielWinata/week01/GradingSystem.kt
@@ -0,0 +1,7 @@
+package oop_00000105533_YehezkielWinata.week01
+fun main () {
+    var name: String = "Yehezkiel Winata"
+    var Grade: Int = 90
+
+    println("Nama" + name + ", Nilai " + Grade)
+}
