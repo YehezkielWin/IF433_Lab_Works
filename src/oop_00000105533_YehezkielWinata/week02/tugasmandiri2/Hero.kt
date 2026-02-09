2d13e7d906fc82e066b461be4534a9c7ef834928
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-09 16:14:50 +0700

    week02: (task 2) create Hero class

---
+++ src/oop_00000105533_YehezkielWinata/week02/tugasmandiri2/Hero.kt
@@ -0,0 +1,15 @@
+package oop_00000105533_YehezkielWinata.week02.tugasmandiri2
+
+class Hero (var Name: String, var hp: Int = 100, var baseDamage: Int){
+    fun attack(targetName: String){
+        println("$Name menebas $targetName!")
+    }
+
+    fun takeDamage(damage: Int): Int{
+        return if (hp <= 0){
+             0
+        } else {
+            hp - damage
+        }
+    }
+}
