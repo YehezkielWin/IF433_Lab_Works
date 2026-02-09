cc9310ce1b95dfec4d0b245a5d3b42713afbf9a3
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-09 15:44:02 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000105533_YehezkielWinata/week02/tugasmandiri1/Loan.kt
@@ -0,0 +1,16 @@
+package oop_00000105533_YehezkielWinata.week02.tugasmandiri1
+
+class Loan(
+    val BookTitle: String,
+    val borrrower: String,
+    var loanDuration: Int = 1)
+{
+    fun calculateFine(): Int {
+        return if (loanDuration > 3) {
+            (loanDuration - 3) * 2000
+        } else {
+            0
+        }
+    }
+}
+
