214e935f629bbd676f6ebf2c57f100ae337526bd
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-09 15:15:17 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000105533_YehezkielWinata/week02/student.kt
+++ src/oop_00000105533_YehezkielWinata/week02/student.kt
@@ -1,6 +1,6 @@
 package oop_00000105533_YehezkielWinata.week02
 
-class Student (val name: String, val nim: String, var major: String)
+class Student (val name: String, val nim: String, var major: String, var gpa: Double = 0.0)
     {
         init {
             if (nim.length != 5){
