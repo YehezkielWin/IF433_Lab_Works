b1aecc770117f73d0f09e22ae9af5d8f6f79af8b
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-09 15:28:37 +0700

    week02: implement selection logic for constructors

--- src/oop_00000105533_YehezkielWinata/week02/Main.kt
+++ src/oop_00000105533_YehezkielWinata/week02/Main.kt
@@ -11,7 +11,7 @@ fun main(){
     val name = scanner.nextLine()
 
     print("Masukkan Nim (Wajib 5 Karakter): ")
-    val nim = scanner.nextLine()
+    val nim = scanner.next()
 
     scanner.nextLine()
 
@@ -23,5 +23,21 @@ fun main(){
 
         val s1 = Student(name, nim, major)
         println("Status: Pendaftaran Selesai")
+    }
+
+    print("Pilih Jalur(1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if (type == 1){
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+    }else if (type == 2){
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+    }else {
+        println("Pilihan ngawur, pendaftaran batal!")
     }
 }
