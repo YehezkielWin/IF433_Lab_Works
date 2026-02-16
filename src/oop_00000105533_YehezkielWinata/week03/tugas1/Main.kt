08ac0a67522982940b54a6014ade0a1ddb2c798f
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-16 15:42:55 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000105533_YehezkielWinata/week03/tugas1/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000105533_YehezkielWinata.week03.tugas1
+
+fun main(){
+    var weapon = Weapon("Yehezkiel", -51, "")
+    println("${weapon.Damage}")
+    println("${weapon.Tier}")
+    var weapon_coba = Weapon("Yehezkiel", 9999, "")
+    println("${weapon_coba.Damage}")
+    println("${weapon_coba.Tier}")
+}
--- src/oop_00000105533_YehezkielWinata/week03/tugas1/Weapon.kt
+++ src/oop_00000105533_YehezkielWinata/week03/tugas1/Weapon.kt
@@ -1,29 +1,37 @@
 package oop_00000105533_YehezkielWinata.week03.tugas1
 
-class Weapon (var Name: String, Damage: Int, Tier: String){
+class Weapon(var Name: String, damage: Int, tier: String){
 
-    var Damage: Int = Damage
-        set(Value){
-            if (Value < 0){
+    var Damage: Int =
+        if (damage < 0) {
+            println("Damage Tidak boleh negatif!")
+            0
+        } else if (damage > 1000) {
+            println("Damage Overpowered")
+            1000
+        } else {
+            damage
+        }
+        set(value){
+            if (value < 0){
                 println("Damage Tidak boleh negatif!")
                 field = 0
-            } else if (Value > 1000){
+            } else if (value > 1000){
                 println("Damage Overpowered")
                 field = 1000
             } else {
-                field = Value
+                field = value
             }
         }
 
-    var Tier: String = Tier
+    val Tier: String
         get(){
-            if (Damage > 800){
-                return "Legendary"
+            return if (Damage > 800){
+                "Legendary"
             } else if (Damage > 500) {
-                return "Epic"
+                "Epic"
             } else {
-                return "Common"
+                "Common"
             }
         }
-
 }
