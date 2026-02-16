5338b0efd7110a9964b3a074d28182d46fa5510f
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-16 14:57:38 +0700

    week03: test all encapsulation logic in main

--- src/oop_00000105533_YehezkielWinata/week03/Main.kt
+++ src/oop_00000105533_YehezkielWinata/week03/Main.kt
@@ -2,10 +2,16 @@ package oop_00000105533_YehezkielWinata.week03
 
 fun main(){
     val e = Employee("Budi")
-    println("---Test Setter---")
-    e.salary = -5000
-    println(e.salary)
 
-    e.salary = 5000
-    println(e.salary)
+    //1. test validasi salary
+    e.salary = -1000 //harusnya print error
+    e.salary = 1000
+    println("Gaji: ${e.salary}")
+
+    //2. test encapsulation
+    e.increasePerformance()
+    //e.performancerating = 5
+
+    //3. test computed property
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
