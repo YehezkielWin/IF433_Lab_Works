c1b01a0af0804a2b6a20dbf50bb7c3f68cbdc078
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-16 14:58:09 +0700

    week03: cleanup code

--- src/oop_00000105533_YehezkielWinata/week03/Main.kt
+++ src/oop_00000105533_YehezkielWinata/week03/Main.kt
@@ -2,16 +2,11 @@ package oop_00000105533_YehezkielWinata.week03
 
 fun main(){
     val e = Employee("Budi")
-
-    //1. test validasi salary
-    e.salary = -1000 //harusnya print error
+    e.salary = -1000
     e.salary = 1000
     println("Gaji: ${e.salary}")
 
-    //2. test encapsulation
     e.increasePerformance()
-    //e.performancerating = 5
 
-    //3. test computed property
     println("Pajak yang harus dibayar: ${e.tax}")
 }
