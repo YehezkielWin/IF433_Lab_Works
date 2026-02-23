089281f74b21c8d7b54f322792a0fa1cfb8c611d
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-02-23 13:37:16 +0700

    week04: create basic Vehicle parent class

--- src/oop_00000105533_YehezkielWinata/week03/tugas2/Main.kt
+++ src/oop_00000105533_YehezkielWinata/week03/tugas2/Main.kt
@@ -1,2 +1,13 @@
 package oop_00000105533_YehezkielWinata.week03.tugas2
 
+fun main(){
+    var player = Player("Yehezkiel")
+    //player.xp = 30 error karena private
+    player.addXp(50)
+    println ("Masih Level ${player.level}")
+
+    player.addXp(60)
+    println("Level anda ${player.level}")
+
+
+}
