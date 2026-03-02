f24e4a496e454a37a9905e4042d14a5751033452
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-02 15:15:28 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000105533_YehezkielWinata/week05/Tugas1/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_00000105533_YehezkielWinata.week05.Tugas1
+
+class MathHelper {
+    fun hitungLuas(sisi: Int): Int{
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int): Int{
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double): Double{
+        return 3.14 * jariJari * jariJari
+    }
+}
