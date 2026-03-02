7edf1b27064b6a481457fb0cc1a129e3f235ebee
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-02 15:19:12 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_00000105533_YehezkielWinata/week05/Tugas1/Main.kt
@@ -0,0 +1,9 @@
+package oop_00000105533_YehezkielWinata.week05.Tugas1
+
+fun main(){
+    val Matematika = MathHelper()
+
+    println(Matematika.hitungLuas(9))
+    println(Matematika.hitungLuas(7, 5))
+    println(Matematika.hitungLuas(8.0))
+}
