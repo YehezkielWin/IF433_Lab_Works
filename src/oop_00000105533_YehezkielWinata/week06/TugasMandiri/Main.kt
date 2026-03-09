493e330749890c83dccf41cdd110fde855e973c2
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-09 16:31:03 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/Main.kt
+++ src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/Main.kt
@@ -1,7 +1,16 @@
 fun main(){
-    
+    val hub = SmartHomeHub()
     val lampu = SmartLamp("0101","Ruang Tamu")
     val spiker = SmartSpeaker("0202","Google Nest Dapur")
     val sisitipi = SmartCCTV("0303","Ezviz Garasi")
 
+    hub.addDevice(lampu)
+    hub.addDevice(spiker)
+    hub.addDevice(sisitipi)
+
+    println("Aktivasi mode keamanan...")
+    hub.activateSecurityMode()
+
+    println("\nMematikan semua perangkat yang bisa di-switch off...")
+    hub.turnOffAllSwitches().
 }
