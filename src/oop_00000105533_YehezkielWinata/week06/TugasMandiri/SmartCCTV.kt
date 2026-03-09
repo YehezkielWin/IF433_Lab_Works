53271ed9263c7c2df469953401bd413b8c5654f0
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-09 16:15:41 +0700

    week06: (task) create SmartCCTV with compound interfaces

--- src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/SmartCCTV.kt
+++ src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/SmartCCTV.kt
@@ -1,13 +1,19 @@
-class SmartLamp(
+class SmartCCTV(
     override val id: String,
     override val name: String
-) : SmartDevice, Switchable {
+) : SmartDevice, Switchable, Recordable {
 
     override fun turnOn() {
         println("Smart Lamp menyala dengan kecerahan sedang.")
+        startRecord()
     }
 
     override fun turnOff() {
         println("Smart Lamp dimatikan.")
+        stopRecord()
+    }
+
+    override fun startRecord() {
+        println("$name mulai merekam...")
     }
 }
--- src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/SmartSpeaker.kt
+++ src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/SmartSpeaker.kt
@@ -1,5 +1,17 @@
-class SmartSpeaker : SmartDevice, Switchable{
-    fun playMusic(song: String){
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+     override fun turnOn() {
+        println("Smart Lamp menyala dengan kecerahan sedang.")
+    }
+
+    override fun turnOff() {
+        println("Smart Lamp dimatikan.")
+    }
+
+    fun playMusic(song: String) {
         println("Memutar lagu $song dari spotify")
     }
 }
