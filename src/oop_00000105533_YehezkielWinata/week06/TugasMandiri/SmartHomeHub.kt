63addcc585ae25ebafcbb34882532ba883bed151
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-09 16:23:41 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/SmartHomeHub.kt
+++ src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/SmartHomeHub.kt
@@ -2,11 +2,21 @@ class SmartHomeHub(val devices = mutableListOf<SmartDevice>()){
     fun addDevice(device: SmartDevice){
         devices.add(device)
     }
-    
+
     fun turnOffAllSwitches(){
         for (device in devices){
             if (device is switchable){
                 device.turnOff()
+            }
+        }
+    }
+
+    fun activateSecurityMode(){
+        for (device in devices){
+            if (device is Recordable){
+                device.startRecord()
+            }else if (devise is SmartSpeaker){
+                device.playMusic("Sirine Peringatan")
             }
         }
     }
