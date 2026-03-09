77038de38ed7eef51cd5ebff679845e418e1acad
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-09 16:27:41 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000105533_YehezkielWinata/week06/TugasMandiri/Main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    
+    val lampu = SmartLamp("0101","Ruang Tamu")
+    val spiker = SmartSpeaker("0202","Google Nest Dapur")
+    val sisitipi = SmartCCTV("0303","Ezviz Garasi")
+
+}
