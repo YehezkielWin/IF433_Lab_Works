cdc5520805fab20240627ece94746ad24c572afc
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-16 15:49:22 +0700

    week07: implement copy and destructuring on data class

--- src/oop_00000105533_YehezkielWinata/week07/Latihan2/Main.kt
+++ src/oop_00000105533_YehezkielWinata/week07/Latihan2/Main.kt
@@ -9,3 +9,9 @@ println("\n=== TEST DATA CLASS ===")
     val data2 = DataUser("Bob", 30)
     println(data1) //akan mencetak isi data dengan format yang rapi
     println("Sama? ${data1 == data2}") //true karena membandingkan isi data, bukan referensi objek
+
+val data3 = data1.copy(age = 23) 
+    println("Hasil Copy: $data3") 
+
+val (userName, userAge) = data1 
+    println("Destructured: Name = $userName, Age = $userAge")
