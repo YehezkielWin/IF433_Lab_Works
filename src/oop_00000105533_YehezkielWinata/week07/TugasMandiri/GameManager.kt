f5a0b81b46d24099bffd0e631493c7d1bf3568b8
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-16 15:58:54 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_00000105533_YehezkielWinata/week07/TugasMandiri/GameManager.kt
@@ -0,0 +1,13 @@
+object GameManager {
+
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
