5100ab5692d4584f27895d67abf60667673d7736
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-16 16:20:01 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000105533_YehezkielWinata/week07/TugasMandiri/Main.kt
+++ src/oop_00000105533_YehezkielWinata/week07/TugasMandiri/Main.kt
@@ -1,4 +1,12 @@
 fun main(){
     GameManager.startGame()
     GameManager.startGame() 
+
+    println("Drop Chance Legendary : ${itemRarity.LEGENDARY.dropChance}%")
+
+    val starterSword = Weapon.forgeStarterSword()
+    println("Senjata awal pemain:")
+    println("Nama: ${starterSword.name}")
+    println("Damage: ${starterSword.damage}")
+    println("Rarity: ${starterSword.rarity}")
 }
