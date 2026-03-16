e1dff04ea0c4540c4207a417f0c65b9b6a3da316
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-03-16 15:51:45 +0700

    week07: create ApiResponse sealed class

--- src/oop_00000105533_YehezkielWinata/week07/Latihan2/SystemStates.kt
+++ src/oop_00000105533_YehezkielWinata/week07/Latihan2/SystemStates.kt
@@ -3,3 +3,9 @@ enum class AppState {
     RUNNING,
     STOPPED
 }
+
+sealed class ApiResponse {
+    data class Success(val data: String) : ApiResponse()
+    data class Error(val message: String) : ApiResponse()
+    object Loading : ApiResponse()
+}
