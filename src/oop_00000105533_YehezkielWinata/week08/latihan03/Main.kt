70db8074e87459b0cb8f7d46a8bab182f23c397c
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-13 13:46:09 +0700

    week08: create mixed data collection

---
+++ src/oop_00000105533_YehezkielWinata/week08/latihan03/Main.kt
@@ -0,0 +1,9 @@
+fun main(){
+    println("\n=== TEST SAFE CASTING ===")
+    val mixedData: List<Any> = listOf("Smartphone", 
+    1500000, 
+    UserProfile("Andi", null),
+    "Laptop", 
+    4500000.0
+    )
+}
