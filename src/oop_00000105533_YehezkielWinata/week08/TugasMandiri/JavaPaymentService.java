7bcdf96e609cf8919d30fe5c94c96bd2f5740563
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-13 14:57:36 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000105533_YehezkielWinata/week08/TugasMandiri/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000105533_YehezkielWinata.week08.TugasMandiri;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
