87f537396f2733f37cc0e7ef8d4cbd7f6bd92954
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-13 13:58:41 +0700

    week08: test safe casting in main

--- src/oop_00000105533_YehezkielWinata/week08/latihan02/OrderModels.kt
+++
@@ -1,5 +0,0 @@
-class City(val name: String)
-class Address(val city: City?)
-class DeliveryDetails(val address: Address?)
-class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
-
