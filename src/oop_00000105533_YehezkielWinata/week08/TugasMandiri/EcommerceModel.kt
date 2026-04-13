8a1a0275248d85662eb6809d61295068bb276490
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-13 15:27:18 +0700

    week08: (task) test full e-commerce pipeline in mainbeneran

---
+++ src/oop_00000105533_YehezkielWinata/week08/TugasMandiri/EcommerceModel.kt
@@ -0,0 +1,13 @@
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
