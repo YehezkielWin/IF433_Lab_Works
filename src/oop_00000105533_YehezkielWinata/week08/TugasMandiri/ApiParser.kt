c05d9e89337b9bed3b3c00f27f2ecf08410dbd82
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-13 15:26:59 +0700

    week08: (task) test full e-commerce pipeline in main

--- src/oop_00000105533_YehezkielWinata/week08/TugasMandiri/ApiParser.kt
+++ src/oop_00000105533_YehezkielWinata/week08/TugasMandiri/ApiParser.kt
@@ -1,33 +1,46 @@
-fun parseProduct(rawJson: Map<String, Any?>): Product? {
-    val id = requireNotNull(rawJson["id"] as? String) {
-        "API Invalid: Missing ID"
-    }
+import oop_00000105533_YehezkielWinata.week08.TugasMandiri.JavaPaymentService
+class ApiParser {
 
-    val name = requireNotNull(rawJson["name"] as? String) {
-        "API Invalid: Missing Name"
-    }
-
-    val type = rawJson["type"] as? String
-
-    return when (type) {
-        "ELECTRONIC" -> {
-            val warranty = rawJson["warranty"] as? Int ?: 12
-            Electronic(id, name, warranty)
+    fun parseProduct(rawJson: Map<String, Any?>): Product? {
+        val id = requireNotNull(rawJson["id"] as? String) {
+            "API Invalid: Missing ID"
         }
-        "CLOTHING" -> {
-            val size = rawJson["size"] as? String ?: "All Size"
-            Clothing(id, name, size)
-        }
-        else -> null
-    }
-}
 
-fun checkout(product: Product) {
+        val name = requireNotNull(rawJson["name"] as? String) {
+            "API Invalid: Missing Name"
+        }
+
+        val type = rawJson["type"] as? String
+
+        return when (type) {
+            "ELECTRONIC" -> {
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                Electronic(id, name, warranty)
+            }
+            "CLOTHING" -> {
+                val size = rawJson["size"] as? String ?: "All Size"
+                Clothing(id, name, size)
+            }
+            else -> null
+        }
+    }
+
+    fun checkout(product: Product) {
     val id = when (product) {
         is Electronic -> product.id
         is Clothing -> product.id
     }
 
+    when (product) {
+        is Electronic -> {
+            println("${product.name} (Warranty ${product.warrantyMonths})")
+        }
+        is Clothing -> {
+            println("${product.name} (Size ${product.size})")
+        }
+    }
+
     val transactionId = JavaPaymentService.processPayment(id)!!
     println(transactionId)
+}
 }
--- src/oop_00000105533_YehezkielWinata/week08/TugasMandiri/EcommerceModels.kt
+++
@@ -1,4 +0,0 @@
-sealed class Product 
-data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
-data class Clothing(val id: String, val name: String, val size: String) : Product()
-
