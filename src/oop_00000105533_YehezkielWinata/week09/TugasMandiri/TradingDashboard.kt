fd4b6b4262e974ac4c50a71c79ca7737bb483a19
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-20 13:46:22 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000105533_YehezkielWinata/week09/TugasMandiri/TradingDashboard.kt
@@ -0,0 +1,9 @@
+fun main(){
+    val tradeHistory = listOf(TradeLog("BTCUSDT", "LONG", 10, 12.5, "OPEN"),
+        TradeLog("ETHUSDT", "SHORT", 5, -3.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 20, 8.1, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 15, -6.7, "OPEN"),
+        TradeLog("BNBUSDT", "LONG", 8, 4.3, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 12, -1.5, "OPEN")
+    )
+}
