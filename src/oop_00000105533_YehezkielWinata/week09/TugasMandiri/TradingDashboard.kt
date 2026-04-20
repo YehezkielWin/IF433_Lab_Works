ce4ccdaf4e1ecfb7e86a73cb2ba224cadad7cdde
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-20 13:50:17 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_00000105533_YehezkielWinata/week09/TugasMandiri/TradingDashboard.kt
+++ src/oop_00000105533_YehezkielWinata/week09/TugasMandiri/TradingDashboard.kt
@@ -1,9 +1,17 @@
 fun main(){
-    val tradeHistory = listOf(TradeLog("BTCUSDT", "LONG", 10, 12.5, "OPEN"),
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 10, 12.5, "OPEN"),
         TradeLog("ETHUSDT", "SHORT", 5, -3.2, "CLOSED"),
         TradeLog("BTCUSDT", "SHORT", 20, 8.1, "CLOSED"),
         TradeLog("ETHUSDT", "LONG", 15, -6.7, "OPEN"),
         TradeLog("BNBUSDT", "LONG", 8, 4.3, "CLOSED"),
         TradeLog("BTCUSDT", "LONG", 12, -1.5, "OPEN")
     )
+
+    println("=== Pipeline 1 ===")
+    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+    println("Closed Trades:")
+    closedTrades.forEach { println(it) }
+
+   
 }
