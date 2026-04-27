322826f262af876a6cff4b1d2aaa6c44d73ec73e
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-04-27 12:57:20 +0700

    week10: (task) initialize and populate transaction repository

--- src/oop_00000105533_YehezkielWinata/week10/TugasMandiri/CryptoDashboard.kt
+++ src/oop_00000105533_YehezkielWinata/week10/TugasMandiri/CryptoDashboard.kt
@@ -11,4 +11,8 @@ fun main() {
     response.data.forEach { coin ->
         println("Coin: ${coin.name}, Balance: ${coin.balance}")
     }
+
+    val txRepo = WalletRepository<Transaction>()
+    txRepo.add(Transaction("TX001", 100.0))
+    txRepo.add(Transaction("TX002", 50.0))
 }
