b4fc0a080159fd8e0b11ee9d5f53e8810ce8e7f7
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-05-04 12:55:45 +0700

    week11: test extension functions in main

---
+++ src/oop_00000105533_YehezkielWinata/week11/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+    
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
