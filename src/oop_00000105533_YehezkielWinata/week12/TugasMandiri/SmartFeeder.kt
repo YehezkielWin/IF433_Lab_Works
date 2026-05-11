7d1508760b5baac7cd3bc30c8274fa9e96caf171
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-05-11 17:08:41 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000105533_YehezkielWinata/week12/TugasMandiri/SmartFeeder.kt
+++ src/oop_00000105533_YehezkielWinata/week12/TugasMandiri/SmartFeeder.kt
@@ -1,3 +1,14 @@
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0 gr" }
+
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
+    return availableGram - requestedGram
 }
