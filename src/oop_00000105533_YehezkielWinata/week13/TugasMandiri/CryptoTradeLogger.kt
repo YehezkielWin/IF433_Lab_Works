a80d6c7957422102b2359eae9b678d43e9f59c5e
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-05-18 14:42:23 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_00000105533_YehezkielWinata/week13/TugasMandiri/CryptoTradeLogger.kt
@@ -0,0 +1,10 @@
+import java.io.File
+import java.io.FileNotFoundException
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
