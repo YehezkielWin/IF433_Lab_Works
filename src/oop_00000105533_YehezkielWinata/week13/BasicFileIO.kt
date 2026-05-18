2fdff47d57ff5b0cb3a89b264b633672421899cf
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-05-18 14:33:32 +0700

    week13: implement basic writeText function

---
+++ src/oop_00000105533_YehezkielWinata/week13/BasicFileIO.kt
@@ -0,0 +1,10 @@
+import java.io.File
+
+fun main() {
+    println("=== TEST WRITE TEXT ===")
+    val file = File("notes.txt")
+    
+    // Menulis teks awal (akan menimpa jika file sudah ada)
+    file.writeText("Line 1: Inisialisasi sistem.\n")
+    println("File berhasil dibuat dan ditulis.")
+}
