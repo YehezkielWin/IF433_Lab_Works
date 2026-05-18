5a21a8ef1056798f1159b696dc28cc1f0e1fe761
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-05-18 14:34:31 +0700

    week13: implement appendText operation

--- src/oop_00000105533_YehezkielWinata/week13/BasicFileIO.kt
+++ src/oop_00000105533_YehezkielWinata/week13/BasicFileIO.kt
@@ -7,4 +7,7 @@ fun main() {
     // Menulis teks awal (akan menimpa jika file sudah ada)
     file.writeText("Line 1: Inisialisasi sistem.\n")
     println("File berhasil dibuat dan ditulis.")
+
+    file.appendText("Line 2: Menambahkan konfigurasi baru.\n")
+    println("Teks berhasil di-append.")
 }
