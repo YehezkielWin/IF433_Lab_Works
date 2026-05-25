72b30ae67d579c70f645d49be75d7d42f9fbff07
YehezkielWin <yehezkiel.winata06@gmail.com>
2026-05-25 13:27:06 +0700

    week14: (task) implement PricingStrategy interface for OCP

--- src/oop_00000105533_YehezkielWinata/week14/TugasMandiri/EcommerceTask.kt
+++ src/oop_00000105533_YehezkielWinata/week14/TugasMandiri/EcommerceTask.kt
@@ -3,27 +3,21 @@ package oop_001_johnthor.week14
 import java.io.File
 
 class BadOrderProcessor {
-    // VIOLATION: Hardcoded File I/O (DIP), Melakukan kalkulasi, I/O, Notifikasi sekaligus (SRP)
     private val file = File("orders.csv")
 
     fun processOrder(itemName: String, basePrice: Double, customerType: String) {
-        // VIOLATION: Kaku jika ada tipe customer/diskon baru di masa depan (OCP)
         val finalPrice = when (customerType) {
             "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.90 // Diskon 10%
+            "VIP" -> basePrice * 0.90
             else -> basePrice
         }
 
         println("Memproses pesanan $itemName seharga $finalPrice")
 
-        // VIOLATION SRP/DIP: Menulis file langsung di class bisnis
         file.appendText("$itemName, $finalPrice, $customerType\n")
 
-        // VIOLATION SRP/DIP: Notifikasi terikat kuat dengan sistem order
         println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
     }
-
-    //INI MASIH VIOLATION SEMUA
 }
 
 interface OrderRepository {
@@ -35,7 +29,7 @@ class CsvOrderRepository : OrderRepository {
     
     override fun saveOrder(itemName: String, finalPrice: Double, customerType: String) {
         file.bufferedWriter().use { writer ->
-            file.appendText("$itemName, $finalPrice, $customerType\n")
+            writer.append("$itemName, $finalPrice, $customerType\n")
         }
     }
 }
@@ -60,6 +54,32 @@ class SafeOrderProcessor(
             "VIP" -> basePrice * 0.90
             else -> basePrice
         }
+
+        println("Memproses pesanan $itemName seharga $finalPrice")
+        
+        repo.saveOrder(itemName, finalPrice, customerType)
+        notifier.sendNotification("Pesanan $itemName Anda telah dikonfirmasi!")
+    }
+}
+
+interface PricingStrategy {
+    fun calculate(price: Double): Double
+}
+
+class RegularPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price
+}
+
+class VipPricing : PricingStrategy {
+    override fun calculate(price: Double): Double = price * 0.90
+}
+
+class FinalSafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+    fun processOrder(itemName: String, basePrice: Double, strategy: PricingStrategy, customerType: String) {
+        val finalPrice = strategy.calculate(basePrice)
 
         println("Memproses pesanan $itemName seharga $finalPrice")
         
